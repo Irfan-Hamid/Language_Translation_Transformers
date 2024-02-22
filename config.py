1a0885d28cd1b9b04e914e03ebc1be9b28655194
Irfan Hamid <irfanhamid19@gmail.com>
2024-02-22 22:20:19 +0000

    config changes

--- config.py
+++ config.py
@@ -18,13 +18,12 @@ def get_config():
     }
 
 def get_weights_file_path(config, epoch: str):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_folder = config['model_folder']
     model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path('.') / model_folder / model_filename)
+    return str(Path(model_folder) / model_filename)
 
-# Find the latest weights file in the weights folder
 def latest_weights_file_path(config):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_folder = config['model_folder']  # Use the updated model_folder directly from config
     model_filename = f"{config['model_basename']}*"
     weights_files = list(Path(model_folder).glob(model_filename))
     if len(weights_files) == 0:
