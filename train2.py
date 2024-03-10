e1b0234f1bff071524a41c589e2ee095cbedd400
Irfan Hamid <irfanhamid19@gmail.com>
2024-03-10 03:25:05 +0000

    10_(5)

--- train2.py
+++ train2.py
@@ -94,7 +94,10 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
 
             source_text = batch["src_text"][0]
             target_text = batch["tgt_text"][0]
-            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+            
+            model_out_ids = model_out.squeeze().cpu().tolist()
+            # Decode the token IDs to text
+            model_out_text = tokenizer_tgt.decode(model_out_ids, skip_special_tokens=True)
 
             source_texts.append(source_text)
             expected.append(target_text)
